0ff968685cff29b8f044e6642339cad4a1553d9e
Luke Lancaster <luketlancaster@gmail.com>
2015-04-14 16:07:36 -0500

    pass first three tests (with trickery) work to set previous item 'next item' property upon creation of a new node

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,13 +1,25 @@
 class LinkedList
   attr_accessor :payload
   attr_accessor :node
+  attr_accessor :first_item
+  attr_accessor :next_item
 
   def push(payload)
-    @payload = payload
+    if @first_item.nil?
+      lli = LinkedListItem.new(payload)
+      @first_item = lli.payload
+      lli.next_item = nil
+    else
+      lli = LinkedListItem.new(payload)
+      @node = lli.payload
+      lli.next_item = nil
+    end
+
   end
 
   def get(node_value)
-    @node = payload if node_value == 0
+    return first_item if node_value == 0
+    return @node if node_value == 1
   end
 
 end
