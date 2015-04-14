c35c29be5eca3013659094d351d396a66c4ac615
Luke Lancaster <luketlancaster@gmail.com>
2015-04-13 22:45:55 -0500

    reach comparison tests

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,9 +1,31 @@
 class LinkedListItem
   include Comparable
-  attr_reader :payload
+  attr_reader :payload, :next_item, :name
 
-  def initialize(data)
-    @payload = data
+  def >(other)
+    if self.name > other.name
+      return true
+    end
   end
 
-end
+  def initialize(payload)
+    @payload = payload
+  end
+
+  def next_item=(next_item)
+    if next_item == self
+      raise ArgumentError, "Can't set self as next item"
+    else
+      @next_item = next_item
+    end
+  end
+
+  def last?
+    if self.next_item == nil
+      return true
+    else
+      return false
+    end
+  end
+
+  end
