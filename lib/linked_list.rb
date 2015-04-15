cc117592eeed46a13a74c5bb452b75f81a3a3fc1
Luke Lancaster <luketlancaster@gmail.com>
2015-04-15 11:30:57 -0500

    pass tests using methods described in class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,25 +1,29 @@
 class LinkedList
-  attr_accessor :payload
-  attr_accessor :node
-  attr_accessor :first_item
-  attr_accessor :next_item
+
+  def get(nth_item)
+    if nth_item == 0
+      @first_item.payload
+    else
+      count = 0
+      current_node = @first_item
+      while count < nth_item
+        current_node = current_node.next_item
+        count += 1
+      end
+      current_node.payload
+    end
+  end
 
   def push(payload)
     if @first_item.nil?
-      lli = LinkedListItem.new(payload)
-      @first_item = lli.payload
-      lli.next_item = nil
+      @first_item = LinkedListItem.new(payload)
+      @last_item = @first_item
     else
-      lli = LinkedListItem.new(payload)
-      @node = lli.payload
-      lli.next_item = nil
+      @last_item.next_item = LinkedListItem.new(payload)
+      @last_item = @last_item.next_item
     end
 
   end
 
-  def get(node_value)
-    return first_item if node_value == 0
-    return @node if node_value == 1
-  end
 
 end
