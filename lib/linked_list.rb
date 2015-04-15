a6c36c646f6f705e0bb3d273f10ee09b0595114b
Luke Lancaster <luketlancaster@gmail.com>
2015-04-15 11:48:51 -0500

    checking for neg and nonexistant index

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,14 +1,20 @@
 class LinkedList
 
   def get(nth_item)
-    if nth_item == 0
+    if nth_item < 0
+      raise IndexError, "Index cannot be a negative number"
+    elsif nth_item == 0
       @first_item.payload
     else
       count = 0
       current_node = @first_item
       while count < nth_item
+        if current_node.nil?
+          raise IndexError, "This index does not exist"
+        else
         current_node = current_node.next_item
         count += 1
+        end
       end
       current_node.payload
     end
