30a2919f5c28841fba755c39bbc7712e7c0fc845
Luke Lancaster <luketlancaster@gmail.com>
2015-04-16 09:57:46 -0500

    finish ll.rb tests, refactor on printing coming

--- ll.rb
+++ ll.rb
@@ -8,14 +8,37 @@ payloads = ARGV # ARGV holds the command line arguments that were passed in as a
 ll = LinkedList.new
 
 # And, populate it
-#
+
 payloads.each do |payload|
   ll.push(payload)
 end
-# Then, print it out
-#
 
-payloads.each do |payload|
-  print ll.get(payload)
+# Then, print it out
+
+print "* -> "
+
+
+
+
+if ll.size > 0
+  print "#{ll.get(0).inspect} -> "
+end
+if ll.size > 1
+  print "#{ll.get(1).inspect} -> "
+end
+if ll.size > 2
+  print "#{ll.get(2).inspect} -> "
+end
+if ll.size > 3
+  print "#{ll.get(3).inspect} -> "
+end
+if ll.size > 4
+  print "#{ll.get(4).inspect} -> "
+end
+if ll.size > 5
+  print "#{ll.get(5).inspect} -> "
 end
 
+
+
+print "nil"
